2c52887ec095ffbf8d9ea9f13f249173585a8179
jal2003 <85318582+jal2003@users.noreply.github.com>
2022-05-04 19:45:21 -0400

    Create MEDIUM-Sum of List Items - Solved.py

---
+++ MEDIUM-Sum of List Items - Solved.py
@@ -0,0 +1,29 @@
+#Challenge:
+#Sum of List Items
+#Medium
+#Problem Description:
+#Write a program to create a class that will have a method that returns the sum of a student's scores stored in a list.
+
+# create the Student class
+class Student:
+
+    # use the __init__() method to initialize the scores attribute  
+    def __init__(self, scores):
+        self.scores = scores
+  
+    # create the get_scores_sum() method that returns the sum of scores items
+    def get_scores_sum(self): 
+        return sum(self.scores)
+  
+  
+# create the scores variable
+scores = [55, 75, 80, 62, 77]
+
+# create an object of Student by passing scores as an argument
+s1 = Student(scores)
+
+# call the get_scores_sum() method using the s1 object
+total = s1.get_scores_sum()
+
+# print total
+print(total)
