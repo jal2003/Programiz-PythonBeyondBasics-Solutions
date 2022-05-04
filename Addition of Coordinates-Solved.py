0b5e1baaf8e051961f13b09308acb1c4e923a08c
jal2003 <85318582+jal2003@users.noreply.github.com>
2022-05-04 19:27:52 -0400

    Create Addition of Coordinates-Solved.py
    Problem Description (MEDIUM)
    Create a program to find the sum of two coordinates by creating objects.
    
    Create a class
    
    Create a class named Coordinate with attributes x and y.
    Use the __init__() method with two arguments to initialize these attributes.
    The class must also have a method named add_coordinates() that adds x and y attributes of two objects, then creates a new Coordinate object with these values, and returns the new Coordinate object.
    Outside the class
    
    Create two coordinates (objects of the Coordinate class) named c1 and c2.
    The x and y attributes of c1 should be 5 and 6 respectively.
    The x and y attributes of c2 should be 7 and 9 respectively.
    Call add_coordinates() using the c1 object with c2 as an argument, and store the result to the c3 variable.
    Print the x attribute of c3.
    Print the y attribute of c3.

---
+++ Addition of Coordinates-Solved.py
@@ -0,0 +1,27 @@
+
+# create the Coordinate class
+class Coordinate:
+
+    # initialize x and y attributes inside __init__()
+    def __init__(self, x, y): 
+        self.x = x
+        self.y = y
+
+    # define the add_coordinates() method
+    def add_coordinates(self, add):
+        result_add = self.x + add.x
+        result_add2 = self.y + add.y
+        result = Coordinate(result_add, result_add2)
+        return result
+        
+
+# create objects c1 and c2
+c1 = Coordinate(5,6)
+c2 = Coordinate(7,9)
+
+# call the add_coordinates() method
+c3 = c1.add_coordinates(c2)
+
+# print attributes of the c3 object
+print(c3.x)
+print(c3.y)
